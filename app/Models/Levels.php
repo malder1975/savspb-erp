b267cd1a4e357d3018592bbd88aa1ba024756c05
rovnov <malder120875@gmail.com>
2019-05-06 10:16:58 +0300

    project in work

---
+++ app/Models/Levels.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Personal;
+
+class Levels extends Model
+{
+    protected $fillable = [
+        'NAME', 'NOTE'
+    ];
+
+    public $timestamps = false;
+
+    public function personal()
+    {
+        return $this->hasMany(Personal::class);
+    }
+}
